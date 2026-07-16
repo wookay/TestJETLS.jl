99b01d8d4690b241bad62373876759d363ed2e0f
WooKyoung Noh <wookay.noh@gmail.com>
2026-07-16 13:00:20 +0900

    test hover

--- test/jetls/hover.jl
+++ test/jetls/hover.jl
@@ -4,10 +4,26 @@ using Test
 using TestJETLS.Hovers: hover_test
 
 # from JETLS/test/test_hover.jl
-hover_test("""
+
+text = """
+    let x│s = collect(1:10)
+        Any[Core.Const(x) for x in xs]
+    end
+"""
+hover_test(text, "(local) xs")
+
+text = """
+    let xs = c│ollect(1:10)
+        Any[Core.Const(x) for x in xs]
+    end
+"""
+hover_test(text, "collect(1:10)")
+
+text = """
     let xs = collect(1:10)
         Any[Core.Const(x│) for x in xs]
     end
-""", "(local) x")
+"""
+hover_test(text, "(local) x")
 
 end # module test_jetls_hover
